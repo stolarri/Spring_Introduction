3c7e16ad13c1a0ce39c25ee64b43ce23c9986c5d
stolari <mpstolarek@gmail.com>
2019-10-19 11:12:38 +0200

    first commit

---
+++ spring_intro/src/test/java/pl/st/spring/spring_intro/SpringIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.st.spring.spring_intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
