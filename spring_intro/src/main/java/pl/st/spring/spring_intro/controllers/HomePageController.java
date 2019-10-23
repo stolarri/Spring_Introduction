0a76902db0755ca9e0e6452ba1bd9eb45d81dd3d
stolari <mpstolarek@gmail.com>
2019-10-23 17:34:09 +0200

    HomePageController created

---
+++ spring_intro/src/main/java/pl/st/spring/spring_intro/controllers/HomePageController.java
@@ -0,0 +1,9 @@
+package pl.st.spring.spring_intro.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomePageController {
+}
