05df33f015efdd1aaceb8ca3002f3512575fca46
stolari <mpstolarek@gmail.com>
2019-10-23 17:43:34 +0200

    Created GET method to maintain HomePage request

--- spring_intro/src/main/java/pl/st/spring/spring_intro/controllers/HomePageController.java
+++ spring_intro/src/main/java/pl/st/spring/spring_intro/controllers/HomePageController.java
@@ -1,9 +1,30 @@
 package pl.st.spring.spring_intro.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.st.spring.spring_intro.model.domain.Advert;
+import pl.st.spring.spring_intro.model.repositories.AdvertRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomePageController {
+
+    private final AdvertRepository advertRepository;
+
+    @Autowired
+    public HomePageController(AdvertRepository advertRepository) {
+        this.advertRepository = advertRepository;
+    }
+
+    @GetMapping
+    public String prepareHomePage(Model model) {
+        List<Advert> advertList = advertRepository.findAllByOrderByPostedDesc();
+        model.addAttribute("adverts", advertList);
+        return "";
+    }
 }
--- spring_intro/src/main/java/pl/st/spring/spring_intro/model/repositories/AdvertRepository.java
+++ spring_intro/src/main/java/pl/st/spring/spring_intro/model/repositories/AdvertRepository.java
@@ -3,5 +3,9 @@ package pl.st.spring.spring_intro.model.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.st.spring.spring_intro.model.domain.Advert;
 
+import java.util.List;
+
 public interface AdvertRepository extends JpaRepository<Advert,Long> {
+    List<Advert> findAllByOrderByPostedDesc();
+
 }
